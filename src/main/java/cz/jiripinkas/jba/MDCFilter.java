714ab9f0a42ff6b04237d46baa4bc163b7012d86
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-07-27 09:02:02 +0200

    spring boot update + MDC

---
+++ src/main/java/cz/jiripinkas/jba/MDCFilter.java
@@ -0,0 +1,26 @@
+package cz.jiripinkas.jba;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class MDCFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        MDC.put("trackingNumber", UUID.randomUUID().toString());
+        try {
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove("trackingNumber");
+        }
+    }
+}
